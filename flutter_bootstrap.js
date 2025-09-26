20d5b70e8f88149d459e105ae59fa90aa5e96c82
chenbo <cba023@hotmail.com>
2025-09-26 17:50:22 +0800

    [v1.2.12]

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3267601826"
+    serviceWorkerVersion: "349043175"
   }
 });
